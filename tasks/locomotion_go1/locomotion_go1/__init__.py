781d1bff22977f02928a328b1f557b3182979b48
toschilt <lt.toschi@gmail.com>
2025-05-11 17:44:03 -0300

    feat: add tasks folder. Inside of it, all RL projects can be added to execute training or playing

---
+++ tasks/locomotion_go1/locomotion_go1/__init__.py
@@ -0,0 +1,21 @@
+# Copyright (c) 2022-2025, The Isaac Lab Project Developers.
+# All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+
+import gymnasium as gym
+
+##
+# Register Gym environments.
+##
+
+gym.register(
+    id="Go1-Locomotion",
+    entry_point="isaaclab.envs:ManagerBasedRLEnv",
+    disable_env_checker=True,
+    kwargs={
+        "env_cfg_entry_point": f"{__name__}.locomotion_go1_env_cfg:Go1LocomotionEnvCfg",
+        "skrl_cfg_entry_point": f"{__name__}:skrl_ppo_cfg.yaml",
+    },
+)
+print(gym.spec("Go1-Locomotion").kwargs)

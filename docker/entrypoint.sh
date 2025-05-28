25b9fbcc8752797b23778853f86a3305152919f5
toschilt <lt.toschi@gmail.com>
2025-05-28 16:43:50 -0300

    docs: document better the steps in the entrypoint

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
-# Clone this repository if it doesn't exist
 cd ~
+
+# Clone this repository
 git clone https://github.com/toschilt/rl_quadrupeds.git
 cd ${ROS_WS}
+# Copy all ROS 2 packages from the cloned repository to the ROS workspace
 cp -r ~/rl_quadrupeds/ros2/* ./src/
+
+# Install ROS 2 dependencies
 rosdep install --from-paths src --ignore-src -r -y
+# Build the ROS 2 workspace
 colcon build
+
 echo "source ${ROS_WS}/install/setup.bash" >> ~/.bashrc
 source ~/.bashrc
+
 cd /
 bash

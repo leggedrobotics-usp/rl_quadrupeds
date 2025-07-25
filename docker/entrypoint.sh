75b960630bd67882f3bade2cde8b29fbc689e745
toschilt <lt.toschi@gmail.com>
2025-07-25 16:08:33 -0300

    fix: fix the entrypoint and the run_compose bash script to be compatible with the Dockerfile and docker-compose files

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,22 +1,18 @@
 #!/bin/bash
-cd ~
 
-REPO_BRANCH="${REPO_BRANCH:-main}"
+sudo apt update
 
-# Clone this repository
-mkdir rl_quadrupeds
-git clone --branch "$REPO_BRANCH" https://github.com/leggedrobotics-usp/rl_quadrupeds.git /rl_quadrupeds
-cd ${ROS_WS}
-# Copy all ROS 2 packages from the cloned repository to the ROS workspace
-cp -r /rl_quadrupeds/ros2/* ./src/
-
-# Install ROS 2 dependencies
+cd /ros2_ws
+# # Install ROS 2 dependencies
 rosdep install --from-paths src --ignore-src -r -y
-# Build the ROS 2 workspace
+# # Build the ROS 2 workspace
 colcon build
 
+source ${ROS_WS}/install/setup.bash
 echo "source ${ROS_WS}/install/setup.bash" >> ~/.bashrc
 source ~/.bashrc
+
+source /rl_quadrupeds/install.sh install --isaaclab
 
 cd /
 exec bash
--- docker/run_compose.sh
+++ docker/run_compose.sh
@@ -1,24 +1,3 @@
 #!/bin/bash
-
-# Default locations for host cache (edit if different)
-HOST_OV_CACHE="$HOME/.cache/ov"
-HOST_NV_CACHE="$HOME/.nvidia/omniverse"
-
-# If the host cache dirs exist, use them. Otherwise, fall back to Docker volumes.
-if [ -d "$HOST_OV_CACHE" ]; then
-  export OV_CACHE_PATH="$HOST_OV_CACHE"
-else
-  export OV_CACHE_PATH="isaac_cache"
-fi
-
-if [ -d "$HOST_NV_CACHE" ]; then
-  export NV_CACHE_PATH="$HOST_NV_CACHE"
-else
-  export NV_CACHE_PATH="isaac_nv_cache"
-fi
-
-# Optional REPO_BRANCH argument (defaults to 'main' if not specified)
-REPO_BRANCH="${1:-main}"
-
-# Launch the container with an interactive shell and REPO_BRANCH env var
-docker compose run --rm -e REPO_BRANCH="$REPO_BRANCH" ros2_humble_isaaclab bash
+xhost +
+docker compose run --rm --service-ports ros2_humble_isaaclab bash

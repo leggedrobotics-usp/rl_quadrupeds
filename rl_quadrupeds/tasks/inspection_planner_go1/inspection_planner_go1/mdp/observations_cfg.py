12ce221b60b1b5c798a9c96dc5b4fa4f1e9b261c
toschilt <lt.toschi@gmail.com>
2025-08-01 17:21:02 -0300

    feat: adds ComputeObjectRelativePose as an observation term in the Go1-Inspection-Planner task

--- rl_quadrupeds/tasks/inspection_planner_go1/inspection_planner_go1/mdp/observations_cfg.py
+++ rl_quadrupeds/tasks/inspection_planner_go1/inspection_planner_go1/mdp/observations_cfg.py
@@ -11,13 +11,23 @@ from isaaclab.envs.mdp.observations import last_action
 
 from quadrupeds_mdp.observations.ray_caster import lidar_scan_hits_labels_flattened
 from quadrupeds_mdp.observations.inspection.coverage import ObjectInspectionCoverage
+from quadrupeds_mdp.observations.inspection.objects_distance import ComputeObjectRelativePose
 from quadrupeds_mdp.observations.map.scene_map import SceneGroundTruthMap
-from quadrupeds_mdp.observations.position import local_pos_rot
+from quadrupeds_mdp.observations.position import local_viewpoint
 
 @configclass
 class NavObservationsCfg:
     @configclass
     class PolicyCfg(ObsGroup):
+        # Last action taken by the robot
+        actions = ObsTerm(func=last_action, noise=Unoise(n_min=-0.01, n_max=0.01))
+
+        # Current viewpoint of the robot
+        viewpoint = ObsTerm(
+            func=local_viewpoint,
+            params={"asset_cfg": SceneEntityCfg("robot")},
+        )
+
         # LiDAR hits with object labels
         # Continuous, used for navigation in the environment
         lidar_scan_w_labels = ObsTerm(
@@ -25,16 +35,16 @@ class NavObservationsCfg:
             params={"sensor_cfg": SceneEntityCfg("ray_caster")},
         )
 
-        # Coverage inspection term
-        object_coverage = ObsTerm(
+        # Coverage inspection score for all objects
+        object_coverage_score = ObsTerm(
             func=ObjectInspectionCoverage,
             params={
                 "sensor_cfg": SceneEntityCfg("ray_caster"),
             },
         )
 
-        # The ground truth map of the scene
-        gt_map = ObsTerm(
+        # Shows the proportion of the environment explored
+        exploration_proportion = ObsTerm(
             func=SceneGroundTruthMap,
             params={
                 "objects": [
@@ -44,11 +54,12 @@ class NavObservationsCfg:
             }
         )
 
-        actions = ObsTerm(func=last_action, noise=Unoise(n_min=-0.01, n_max=0.01))
-        pos_and_rot = ObsTerm(
-            func=local_pos_rot,
-            params={"asset_cfg": SceneEntityCfg("robot")},
+        # Computes the relative pose of specified objects w.r.t. the robot
+        object_relative_pose = ObsTerm(
+            func=ComputeObjectRelativePose,
+            params={"objects": ["block1", "block2"]}
         )
+
 
         def __post_init__(self):
             self.enable_corruption = True

5594bea07a1b7ff19779694097f03e4beabe297a
toschilt <lt.toschi@gmail.com>
2025-08-01 17:19:27 -0300

    fix: local_viewpoint now returns the robot's viewpoint in the environment's frame properly

--- rl_quadrupeds/quadrupeds_mdp/observations/position.py
+++ rl_quadrupeds/quadrupeds_mdp/observations/position.py
@@ -4,18 +4,12 @@ from isaaclab.assets import RigidObject
 from isaaclab.envs import ManagerBasedEnv
 from isaaclab.managers import SceneEntityCfg
 
-def local_pos_rot(
+def local_viewpoint(
     env: ManagerBasedEnv, 
     asset_cfg: SceneEntityCfg, 
 ) -> torch.Tensor:
-    """Height scan from the given sensor w.r.t. the sensor's frame.
-
-    The provided offset (Defaults to 0.5) is subtracted from the returned values.
-    """
+    """Position and orientation of the asset in the local environment frame."""
     asset: RigidObject = env.scene[asset_cfg.name]
-    pos = asset.data.root_link_state_w[:, :3] - env.scene.env_origins
-    rot = asset.data.root_link_state_w[:, 3:7]  # [batch_size, 4] quaternion
-    return torch.cat([
-        pos, 
-        rot
-    ], dim=-1)  # [batch_size, 7] where last dim is (x, y, z, qx, qy, qz, qw)
+    robot_viewpoint = asset.data.root_link_state_w[:, :7]
+    robot_viewpoint[:, :3] -= env.scene.env_origins
+    return robot_viewpoint

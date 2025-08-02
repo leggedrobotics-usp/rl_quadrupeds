cf5adde52a3287cf200ff7482e1d46a6acc318ab
toschilt <lt.toschi@gmail.com>
2025-08-02 13:45:30 -0300

    feat: implements robot_is_ready_for_new_command, an observation term to indicate to the agent if the robot is ready or not to receive another viewpoint command

---
+++ rl_quadrupeds/quadrupeds_mdp/observations/state.py
@@ -0,0 +1,12 @@
+from isaaclab.envs.manager_based_env import ManagerBasedEnv
+
+def robot_is_ready_for_new_command(
+    env: ManagerBasedEnv
+):
+    """
+    Checks the state of the env variable is_ready_for_new_command.
+    """
+    if not hasattr(env, "is_ready_for_new_command"):
+        raise ValueError("The environment does not have 'is_ready_for_new_command' attribute.")
+    
+    return env.is_ready_for_new_command

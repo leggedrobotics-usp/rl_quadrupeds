1fded3f6523800540af887bfcefdb07f81019282
toschilt <lt.toschi@gmail.com>
2025-08-05 13:25:15 -0300

    fix: tune the rewards for the Go1-Inspection-Planner task

--- rl_quadrupeds/tasks/inspection_planner_go1/inspection_planner_go1/mdp/rewards_cfg.py
+++ rl_quadrupeds/tasks/inspection_planner_go1/inspection_planner_go1/mdp/rewards_cfg.py
@@ -1,16 +1,16 @@
 from isaaclab.managers import RewardTermCfg as RewTerm
 from isaaclab.envs.mdp.rewards import (
-    is_alive
+    is_alive,
+    is_terminated_term
 )
 from isaaclab.utils import configclass
 
 from quadrupeds_mdp.rewards.inspection import (
     get_inspection_action,
     get_overall_inspection_coverage,
-    get_overall_inspection_coverage_gain,
+    MaxCoverageGainReward,
     get_if_inspection_done,
-    get_known_inspection_points,
-    get_unknown_inspection_points,
+    KnownInspectionPointsGainReward,
     MilestoneCoverageReward
 )
 from quadrupeds_mdp.rewards.contact import get_illegal_contact
@@ -25,35 +25,35 @@ from quadrupeds_mdp.rewards.position import (
 
 @configclass
 class RewardsCfg:
-    viewpoint_action_rate_l2 = RewTerm(
-        func=viewpoint_action_rate_l2,
-        weight=-5
-    )
+    # viewpoint_action_rate_l2 = RewTerm(
+    #     func=viewpoint_action_rate_l2,
+    #     weight=-5
+    # )
 
     # penalize_inspection_action = RewTerm(
     #     func=get_inspection_action,
     #     weight=-100
     # )
 
-    overall_inspection_coverage = RewTerm(
-        func=get_overall_inspection_coverage,
-        weight=100.
-    )
+    # overall_inspection_coverage = RewTerm(
+    #     func=get_overall_inspection_coverage,
+    #     weight=100.
+    # )
 
     overall_inspection_coverage_gain = RewTerm(
-        func=get_overall_inspection_coverage_gain,
+        func=MaxCoverageGainReward,
         weight=300.
     )
 
     known_inspection_points = RewTerm(
-        func=get_known_inspection_points,
+        func=KnownInspectionPointsGainReward,
         weight=200.
     )
 
-    env_exploration = RewTerm(
-        func=get_env_exploration_percentage,
-        weight=10
-    )
+    # env_exploration = RewTerm(
+    #     func=get_env_exploration_percentage,
+    #     weight=10
+    # )
 
     current_robot_viewpoint_not_visited = RewTerm(
         func=check_if_current_robot_viewpoint_not_visited,
@@ -61,13 +61,15 @@ class RewardsCfg:
     )
 
     inspection_done = RewTerm(
-        func=get_if_inspection_done,
-        weight=500
+        func=is_terminated_term,
+        weight=500,
+        params={"term_keys": "inspection_done"}
     )
 
     illegal_contact = RewTerm(
-        func=get_illegal_contact,
-        weight=-200
+        func=is_terminated_term,
+        weight=-200,
+        params={"term_keys": "base_contact"}
     )
 
     inspection_milestones_reward = RewTerm(

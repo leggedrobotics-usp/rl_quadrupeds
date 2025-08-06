08f6b2431637a2cbd33b1fc36154f1c15b0576bf
toschilt <lt.toschi@gmail.com>
2025-08-06 14:38:45 -0300

    fix: adds MinDistanceToObjectsAndWalls parameters and tune reward weights

--- rl_quadrupeds/tasks/obstacle_avoidance_go1/obstacle_avoidance_go1/mdp/rewards_cfg.py
+++ rl_quadrupeds/tasks/obstacle_avoidance_go1/obstacle_avoidance_go1/mdp/rewards_cfg.py
@@ -1,12 +1,12 @@
 from isaaclab.managers import RewardTermCfg as RewTerm, SceneEntityCfg
 from isaaclab.envs.mdp.rewards import (
     action_rate_l2,
-    is_terminated
+    is_terminated,
 )
 from isaaclab.utils import configclass
 
 from quadrupeds_mdp.rewards.exploration import (
-    get_min_distance_from_objects_and_walls
+    MinDistanceToObjectsAndWalls
 )
 
 from quadrupeds_mdp.rewards.position import (
@@ -20,39 +20,36 @@ from quadrupeds_mdp.rewards.velocity import (
 
 @configclass
 class RewardsCfg:
-    action_rate_l2 = RewTerm(func=action_rate_l2, weight=-0.2)
+    action_rate_l2 = RewTerm(func=action_rate_l2, weight=-1.0)
 
     stay_away_from_obstacles = RewTerm(
-        func=get_min_distance_from_objects_and_walls,
-        weight=-10,
+        func=MinDistanceToObjectsAndWalls,
+        weight=-50.0,
         params={
             "objects": ["block1", "block2"],
             "walls": ["right_wall", "left_wall", "front_wall", "back_wall"],
+            "safe_distance": 1.0,
+            "near_distance": 0.5,
+            "sharpness": 10.0,
         },
     )
 
     position_tracking = RewTerm(
         func=position_command_error_tanh,
-        weight=8.0,
+        weight=10.0,
         params={"std": 4.0, "command_name": "pose_command"},
     )
 
     position_tracking_fine_grained = RewTerm(
         func=position_command_error_tanh,
-        weight=4.0,
+        weight=5.0,
         params={"std": 0.4, "command_name": "pose_command"},
     )
 
     orientation_tracking = RewTerm(
         func=heading_command_error_abs,
-        weight=-5,
+        weight=-10.0,
         params={"command_name": "pose_command"},
     )
 
-    velocity_aligned_with_goal = RewTerm(
-        func=velocity_aligned_with_goal_reward,
-        weight=2.0,
-        params={"normalize": True, "clip_min": -10.0, "clip_max": 10.0}
-    )
-
-    termination_penalty = RewTerm(func=is_terminated, weight=-50.0)
+    termination_penalty = RewTerm(func=is_terminated, weight=-100.0)

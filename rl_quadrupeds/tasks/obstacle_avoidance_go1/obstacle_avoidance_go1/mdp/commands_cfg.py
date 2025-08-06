29d528814a678aa0918f082d78663ca7bea0b3ca
toschilt <lt.toschi@gmail.com>
2025-08-06 14:38:02 -0300

    fix: changes the pose command sampling class to ObstaclesPose2dCommand - it avoids sampling a position that is occupied by an obstacle

--- rl_quadrupeds/tasks/obstacle_avoidance_go1/obstacle_avoidance_go1/mdp/commands_cfg.py
+++ rl_quadrupeds/tasks/obstacle_avoidance_go1/obstacle_avoidance_go1/mdp/commands_cfg.py
@@ -1,9 +1,9 @@
 import math
 
 from isaaclab.utils import configclass
-from isaaclab.envs.mdp.commands import UniformPose2dCommandCfg
 
 from isaaclab_extensions.ros.subscribers.vel_cmd.vel_cmd_cfg import ROSVelCmdSubscriberCfg
+from quadrupeds_mdp.commands.pose import ObstaclesPose2dCommandCfg
 
 from locomotion_go1.mdp.commands_cfg import CommandsCfg as LocomotionCommandsCfg
 LocomotionCommandsCfg = LocomotionCommandsCfg()
@@ -21,7 +21,7 @@ class CommandsCfg():
     """
     # Removes it from the Locomotion commands because the
     # velocity is already handled by the action cfg.
-    base_velocity = None
+    # base_velocity = None
 
     body_height_orientation_cmd = LocomotionCommandsCfg.body_height_orientation_cmd
     gait_freq_cmd = LocomotionCommandsCfg.gait_freq_cmd
@@ -39,10 +39,11 @@ class CommandsCfg():
     #     resampling_time_range=(0.001, 0.001),
     # )
 
-    pose_command = UniformPose2dCommandCfg(
+    pose_command = ObstaclesPose2dCommandCfg(
         asset_name="robot",
         simple_heading=False,
         resampling_time_range=(20., 20.),
         debug_vis=True,
-        ranges=UniformPose2dCommandCfg.Ranges(pos_x=(-3.5, 3.5), pos_y=(-3.5, 3.5), heading=(-math.pi, math.pi)),
+        ranges=ObstaclesPose2dCommandCfg.Ranges(pos_x=(-3.5, 3.5), pos_y=(-3.5, 3.5), heading=(-math.pi, math.pi)),
+        obstacles=["block1", "block2"],
     )

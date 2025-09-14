b1c2136bec06b035f2397a6d9b6e772313078fbc
toschilt <lt.toschi@gmail.com>
2025-09-14 19:34:45 -0300

    feat: configures skrl SAC, rl_games PPO and rsl_rl PPO into the Go1-Inspection-Planner task

--- rl_quadrupeds/tasks/inspection_planner_go1/inspection_planner_go1/__init__.py
+++ rl_quadrupeds/tasks/inspection_planner_go1/inspection_planner_go1/__init__.py
@@ -17,6 +17,9 @@ gym.register(
     kwargs={
         "env_cfg_entry_point": f"{__name__}.inspection_planner_go1_env_cfg:Go1InspectionPlannerEnvCfg",
         "skrl_cfg_entry_point": f"{__name__}:skrl_ppo_cfg.yaml",
+        # "skrl_cfg_entry_point": f"{__name__}:skrl_sac_cfg.yaml",
+        "rl_games_cfg_entry_point": f"{__name__}:rl_games_ppo_cfg.yaml",
+        "rsl_rl_cfg_entry_point": f"{__name__}.rsl_rl_ppo_cfg:Go1InspectionPlannerPPORunnerCfg",
     },
 )
 print(gym.spec("Go1-Inspection-Planner").kwargs)

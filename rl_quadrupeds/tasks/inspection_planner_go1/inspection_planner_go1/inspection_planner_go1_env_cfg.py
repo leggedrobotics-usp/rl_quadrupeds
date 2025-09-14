67257c87590a73deaf5603c7f1fc00d4323e6c8b
toschilt <lt.toschi@gmail.com>
2025-09-14 19:35:49 -0300

    feat: implements curriculum into the Go1-Inspection-Planner task cfg

--- rl_quadrupeds/tasks/inspection_planner_go1/inspection_planner_go1/inspection_planner_go1_env_cfg.py
+++ rl_quadrupeds/tasks/inspection_planner_go1/inspection_planner_go1/inspection_planner_go1_env_cfg.py
@@ -8,12 +8,13 @@ from .mdp.events_cfg import EventCfg
 from .mdp.observations_cfg import NavObservationsCfg
 from .mdp.rewards_cfg import RewardsCfg
 from .mdp.terminations_cfg import TerminationsCfg
+from .mdp.curriculum_cfg import CurriculumCfg
 
 @configclass
 class Go1InspectionPlannerEnvCfg(ManagerBasedRLEnvCfg):
     scene: Go1InspectionPlannerSceneCfg = Go1InspectionPlannerSceneCfg(
         num_envs=512, 
-        env_spacing=10.0
+        env_spacing=10.
     )
     actions: ActionsCfg = ActionsCfg()
     commands: CommandsCfg = CommandsCfg()
@@ -21,11 +22,12 @@ class Go1InspectionPlannerEnvCfg(ManagerBasedRLEnvCfg):
     observations: NavObservationsCfg = NavObservationsCfg()
     rewards: RewardsCfg = RewardsCfg()
     terminations: TerminationsCfg = TerminationsCfg()
+    curriculum: CurriculumCfg = CurriculumCfg()
 
     def __post_init__(self) -> None:
         # general settings
         self.decimation = 4
-        self.episode_length_s = 60.0
+        self.episode_length_s = 30.
         # viewer settings
         self.viewer.eye = (8.0, 0.0, 5.0)
         # simulation settings

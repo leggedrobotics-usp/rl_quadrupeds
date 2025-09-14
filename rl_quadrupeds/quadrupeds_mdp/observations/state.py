d455f9ef0310a0a67a30bbb6a8d69272ec0e90af
toschilt <lt.toschi@gmail.com>
2025-09-14 19:30:30 -0300

    fix: robot_is_ready_for_new_command now returns explicit floats instead of bools (that were converted implicitly to floats)

--- rl_quadrupeds/quadrupeds_mdp/observations/state.py
+++ rl_quadrupeds/quadrupeds_mdp/observations/state.py
@@ -9,4 +9,4 @@ def robot_is_ready_for_new_command(
     if not hasattr(env, "is_ready_for_new_command"):
         raise ValueError("The environment does not have 'is_ready_for_new_command' attribute.")
     
-    return env.is_ready_for_new_command
+    return env.is_ready_for_new_command.float()
